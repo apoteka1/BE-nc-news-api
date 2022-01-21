5c39a26a5c96a0a6da2e7be51863c51694a996f7
apoteka1 <joseph.ackroyd.1991@gmail.com>
2022-01-21 12:32:10 +0000

    f

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -90,5 +90,5 @@ exports.deleteCommentById = (req, res, next) => {
 };
 
 exports.getApi = (req, res, next) => {
-  res.status(200).send(endpoints).catch(next);
+  res.status(200).send({endpoints}).catch(next);
 };
